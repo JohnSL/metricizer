cc24a975a7f4d93206a87a5c5591c34a6f03ce0a
John Socha-Leialoha <john@socha.com>
2021-07-22 23:27:32 -0700

    First working keypad version.

--- src/main.rs
+++ src/main.rs
@@ -17,22 +17,6 @@ use hal::gpio::gpioa::{PA7, PA8, PA9};
 use hal::gpio::gpiob::{PB5, PB6, PB10};
 use hal::gpio::gpioc::{PC7};
 
-// keypad_struct!{
-//     struct MyKeypad {
-//         rows: (
-//             PB6<Input<PullUp>>,
-//             PB5<Input<PullUp>>,
-//             PB10<Input<PullUp>>,
-//             PA9<Input<PullUp>>,
-//         ),
-//         columns: (
-//             PC7<Output<OpenDrain>>,
-//             PA7<Output<OpenDrain>>,
-//             PA8<Output<OpenDrain>>,
-//         ),
-//     }
-// }
-
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
@@ -50,46 +34,52 @@ fn main() -> ! {
 
     let mut delay = Delay::new(cp.SYST, clocks);
 
-    let mut keypad = MyKeypad::new((
-            gpiob.pb6.into_pull_up_input(&mut gpiob.crl),
-            gpiob.pb5.into_pull_up_input(&mut gpiob.crl),
-            gpiob.pb10.into_pull_up_input(&mut gpiob.crh),
-            gpioa.pa9.into_pull_up_input(&mut gpioa.crh)
-        ),(
-            gpioc.pc7.into_open_drain_output(&mut gpioc.crl),
-            gpioa.pa7.into_open_drain_output(&mut gpioa.crl),
-            gpioa.pa8.into_open_drain_output(&mut gpioa.crh)
-        )
+    let rows = (
+        gpiob.pb10.into_pull_up_input(&mut gpiob.crh),
+        gpioa.pa7.into_pull_up_input(&mut gpioa.crl),
+        gpiob.pb6.into_pull_up_input(&mut gpiob.crl),
+        gpioa.pa9.into_pull_up_input(&mut gpioa.crh)
+
     );
+
+    let cols = (
+        gpioa.pa8.into_open_drain_output(&mut gpioa.crh),
+        gpiob.pb5.into_open_drain_output(&mut gpiob.crl),
+        gpioc.pc7.into_open_drain_output(&mut gpioc.crl),
+    );
+
+    let mut keypad = MyKeypad::new(rows, cols);
 
     loop {
         let f: f32 = 1.2;
-        let v = keypad.read();
+        let v = keypad.read(&mut delay);
+        if v != 0 {
+            delay.delay_ms(f as u16);
+        }
         delay.delay_ms(f as u16);
     }
 }
 
 // Pin      Keypad              Keypad  Pin
 // ----     ------              ------  ---
-// PA7      C2                  C1      PC7
-// PB6      R1                  C2      PA7
-// PC7      C1                  C3      PA8
-// PA9      R4                  R1      PB6
-// PA8      C3                  R2      PB5
-// PB10     R3                  R3      PB10
-// PB5      R2                  R4      PA9
-
+// PA7      R2                  R1      PB10
+// PB6      R3                  R2      PA7
+// PC7      C3                  R3      PB6
+// PA9      R4                  R4      PA9
+// PA8      C1                  C1      PA8
+// PB10     R1                  C2      PB5
+// PB5      C2                  C3      PC7
 pub type KeypadRows = (
-	PB6<Input<PullUp>>,
-	PB5<Input<PullUp>>,
 	PB10<Input<PullUp>>,
+	PA7<Input<PullUp>>,
+	PB6<Input<PullUp>>,
 	PA9<Input<PullUp>>,
 );
 
 pub type KeypadColumns = (
-	PC7<Output<OpenDrain>>,
-	PA7<Output<OpenDrain>>,
 	PA8<Output<OpenDrain>>,
+	PB5<Output<OpenDrain>>,
+	PC7<Output<OpenDrain>>,
 );
 
 pub struct MyKeypad {
@@ -105,7 +95,7 @@ impl MyKeypad {
 	fn read_column(&self) -> u16 {
 		let mut res = 0;
 		
-		if self.rows.0.is_low().unwrap() {
+        if self.rows.0.is_low().unwrap() {
 			res |= 1 << 0;
 		}
 		if self.rows.1.is_low().unwrap() {
@@ -120,22 +110,22 @@ impl MyKeypad {
 		
 		res
 	}
-	
-	pub fn read(&mut self) -> u16 {
+
+	pub fn read(&mut self, delay: &mut Delay) -> u16 {
 		let mut res = 0;
 		
-		self.columns.0.set_low().unwrap();
-		// Add delay here?
+        self.columns.0.set_low().unwrap();
+        delay.delay_ms(10u16);
 		res |= self.read_column() << 0;
 		self.columns.0.set_high().unwrap();
 		
 		self.columns.1.set_low().unwrap();
-		// Add delay here?
+        delay.delay_ms(10u16);
 		res |= self.read_column() << 4;
 		self.columns.1.set_high().unwrap();
 		
 		self.columns.2.set_low().unwrap();
-		// Add delay here?
+        delay.delay_ms(10u16);
 		res |= self.read_column() << 8;
 		self.columns.2.set_high().unwrap();
 		
