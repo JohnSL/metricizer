4eba3090eb805b6a0c84d05f67682846cbf6b60f
John Socha-Leialoha <john@socha.com>
2021-08-07 11:58:30 -0700

    Updated to latest lcd library

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 use core::fmt::Write;
 use embedded_hal::blocking::i2c;
 use heapless::String;
-use lcd_1602_i2c::Lcd;
+use lcd_1602_i2c::{Lcd, Cursor};
 
 pub struct MainApp<I>
 where
@@ -21,7 +21,7 @@ where
         I: i2c::Write,
     {
         let mut app = MainApp {
-            lcd: lcd,
+            lcd,
             entered: String::new(),
             dot: false,
         };
@@ -31,7 +31,7 @@ where
 
     fn init(&mut self) -> Result<(), <I as i2c::Write>::Error> {
         self.lcd.set_rgb(255, 255, 255)?;
-        self.lcd.cursor_on()
+        self.lcd.set_cursor(Cursor::On)
     }
 
     pub fn clear(&mut self) -> Result<(), <I as i2c::Write>::Error> {
@@ -73,10 +73,10 @@ where
         line1.push_str(&self.entered).unwrap();
 
         let cursor = if self.entered.len() > 0 {
-            self.lcd.cursor_on()?;
+            self.lcd.set_cursor(Cursor::On)?;
             self.entered.len() as u8
         } else {
-            self.lcd.cursor_off()?;
+            self.lcd.set_cursor(Cursor::Off)?;
             0
         };
 
@@ -96,14 +96,14 @@ where
         }
 
         // Draw the inches to mm line
-        self.lcd.cursor_position(0, 0)?;
-        self.lcd.print(&line1)?;
+        self.lcd.set_cursor_position(0, 0)?;
+        self.lcd.write_str(&line1)?;
 
         // Draw the mm to inches line
-        self.lcd.cursor_position(0, 1)?;
-        self.lcd.print(&line2)?;
+        self.lcd.set_cursor_position(0, 1)?;
+        self.lcd.write_str(&line2)?;
 
-        self.lcd.cursor_position(cursor, 0)
+        self.lcd.set_cursor_position(cursor, 0)
     }
 
     fn add_input(&self, line: &mut String<16>, input: &String<16>, suffix: &str) {
@@ -113,8 +113,8 @@ where
             write!(line, "{:8}", "0").unwrap_or_default();
         } else {
             let mut number: String<8> = String::new();
-            number.push_str(&input).unwrap_or_default();
-            number.push_str(&suffix).unwrap_or_default();
+            number.push_str(input).unwrap_or_default();
+            number.push_str(suffix).unwrap_or_default();
             write!(line, "{:7}", number).unwrap_or_default();
         }
     }
